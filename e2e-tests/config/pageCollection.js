c6c94028dbc9c71420a24430a6e3e40fbad3d53b
Evelahanti <ewelina.maria.jedrzejczak@gmail.com>
2017-04-20 09:44:29 +0200

    test

--- e2e-tests/config/pageCollection.js
+++ e2e-tests/config/pageCollection.js
@@ -3,6 +3,7 @@ var pageCollection = (function () {
     var mainPage = require('../tests/page/mainPage'),
         orderDiet = require('../tests/page/orderDiet'),
         stdDietForm = require('../tests/page/standardDietForm');
+    //test
 
 
 
